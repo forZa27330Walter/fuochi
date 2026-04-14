e768795e87bf94a8f48af4df287a5ce2e37bc531
forZa27330Walter <allsatone@gmail.com>
2026-04-14 13:17:29 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,25 @@
-const CACHE_NAME = 'fireworks-cache-v1';
-const assets = ['./', './index.html', './manifest.json'];
+const CACHE_NAME = 'fireworks-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './sw.js'
+];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(assets)));
+// Installa il Service Worker e salva i file in cache
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+// Gestisce le richieste quando sei offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
